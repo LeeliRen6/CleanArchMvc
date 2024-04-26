c1a6df6c46aab9e962c78c8a6c6a68b76fcbb893
LeeliRen6 <Leeliren@proton.me>
2024-04-26 11:39:13 -0300

    Criado testes do product

--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -33,7 +33,7 @@ namespace CleanArchMvc.Domain.Entities
         private void ValidateDomain(string name, string description, decimal price, int stock, string image)
         {
             DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name. Name is required");
-            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters");
             DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Invalid description. Description is required");
             DomainExceptionValidation.When(description.Length < 5, "Invalid description, too short");
             DomainExceptionValidation.When(price < 0, "Invalid price value");
