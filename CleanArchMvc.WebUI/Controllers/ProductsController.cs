4b386890faa0014bb08de321b34a47c6621f2e16
LeeliRen6 <Leeliren@proton.me>
2024-05-01 10:18:57 -0300

    Criado pagina de criar produto

--- CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -1,5 +1,9 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Services;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -7,10 +11,17 @@ namespace CleanArchMvc.WebUI.Controllers
     public class ProductsController : Controller
     {
         private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        private readonly IWebHostEnvironment _environment;
 
-        public ProductsController(IProductService productAppService)
+        public ProductsController(IProductService productAppService,
+            ICategoryService categoryService, IWebHostEnvironment environment)
         {
             _productService = productAppService;
+            _categoryService = categoryService;
+            _environment = environment;
+
+
         }
 
         [HttpGet]
@@ -18,6 +29,25 @@ namespace CleanArchMvc.WebUI.Controllers
         {
             var products = await _productService.GetProductsAsync();
             return View(products);
+        }
+        [HttpGet()]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId =
+            new SelectList(await _categoryService.GetCategoriesAsync(), "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProductDTO productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.AddAsync(productDto);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productDto);
         }
     }
 }
