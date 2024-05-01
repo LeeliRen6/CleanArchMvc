a002285823e3b5d60d5663080c0ebc5b7caf2e92
LeeliRen6 <Leeliren@proton.me>
2024-05-01 09:41:18 -0300

    Criado edição de categoria

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -36,5 +36,31 @@ namespace CleanArchMvc.WebUI.Controllers
             
             return View(categoryDTO);
         }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null) return NotFound();
+            var categoyDTO = await _categoryService.GetByIdAsync(id);
+            if (categoyDTO == null) return NotFound();
+            return View(categoyDTO);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryDTO categoryDTO)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryService.UpdateAsync(categoryDTO);
+                }
+                catch (System.Exception)
+                {
+
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryDTO);
+        }
     }
 }
