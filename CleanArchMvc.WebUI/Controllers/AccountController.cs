3af20bba98191f4006116476f7c9f0795f6a1e96
LeeliRen6 <Leeliren@proton.me>
2024-05-02 09:56:37 -0300

    Criado ViewModels e AccountController

---
+++ CleanArchMvc.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthenticate authenticate;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
